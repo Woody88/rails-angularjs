286b5be8c401cf0bf1b688d5a09da5be61a9f21d
Woodson Delhia <woodsondelhia@hotmail.com>
2015-05-24 21:59:45 -0400

    Added post and comments routes and controllers

--- flapper-news/config/routes.rb
+++ flapper-news/config/routes.rb
@@ -3,7 +3,19 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'application#angular'
+    root 'application#angular'
+
+    resources :posts, only: [:create, :index, :show] do
+      resources :comments, only: [:create, :show] do
+        member do 
+          put '/upvote' => 'comments#upvote'
+        end
+      end
+
+      member do 
+          put '/upvote' => 'posts#upvote'
+        end
+    end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
